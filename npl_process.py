4f108341791947cb8225fb58836dafe44f4cff13
Rusakov Aleksey <rusal@bk.ru>
2024-02-08 01:47:11 +0300

    fix

---
+++ npl_process.py
@@ -0,0 +1,95 @@
+﻿import os
+import json
+import nltk
+import pymorphy2
+
+
+def remove_digit(data):
+    str2 = ''
+    for c in data:
+        if c not in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '«', '»', '–', "\""):
+            str2 = str2 + c
+    data = str2
+    return data
+
+
+def remove_punctuation(data):
+    str2 = ''
+    import string
+    pattern = string.punctuation
+    for c in data:
+        if c not in pattern:
+            str2 = str2 + c
+        else:
+            str2 = str2 + ""
+    data = str2
+    return data
+
+
+def remove_stopwords(data):
+    str2 = ''
+    from nltk.corpus import stopwords
+    russian_stopwords = stopwords.words("russian")
+    for word in data.split():
+        if word not in (russian_stopwords):
+            str2 = str2 + " " + word
+    data = str2
+    return data
+
+
+def remove_short_words(data, length=1):
+    str2 = ''
+    for line in data.split("\n"):
+        str3 = ""
+        for word in line.split():
+            if len(word) > length:
+                str3 += " " + word
+        str2 = str2 + "\n" + str3
+    data = str2
+    return data
+
+
+def remove_paragraf_to_lower(data):
+    data = data.lower()
+    data = data.replace('\n', ' ')
+    return data
+
+
+def remove_all(data):
+    data = remove_digit(data)
+    data = remove_punctuation(data)
+    data = remove_stopwords(data)
+    data = remove_short_words(data, length=3)
+    data = remove_paragraf_to_lower(data)
+    return data
+
+def get_normal_form_mas(words):
+    morph = pymorphy2.MorphAnalyzer()
+    result = []
+    for word in words.split():
+        p = morph.parse(word)[0]
+        result.append(p.normal_form)
+    return result
+
+
+def get_normal_form(words):
+    morph = pymorphy2.MorphAnalyzer()
+    p = morph.parse(words)[0]
+    return p.normal_form
+
+def remove_paragraf_and_toLower(text):
+    text = text.lower()
+    text = text.replace('\n', ' ')
+    text = ' '.join([k for k in text.split(" ") if k])
+    return text
+
+
+def nltk_download():
+    nltk.download('stopwords')
+    nltk.download('punkt')
+    
+
+if __name__ == '__main__':
+    nltk_download()
+
+
