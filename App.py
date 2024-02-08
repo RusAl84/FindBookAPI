99d804e5203cac116282ec34f834a962072309da
Rusakov Aleksey <rusal@bk.ru>
2024-02-08 03:08:09 +0300

    final API

---
+++ App.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import npl_process
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def dafault_route():
+    return 'find books API'
+
+# отправка сообщений
+@app.route("/find_book", methods=['POST'])
+def SendMessage():
+    data = request.json
+    print(data)
+    text = data['text']
+    data = npl_process.find_book(text)
+    return data, 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
